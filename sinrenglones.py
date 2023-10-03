5c33bca27643fa251183d1d60bc35f32d9449e39
jonatan1014 <lamotomasrrapidadelmundo@gmail.com>
2023-10-02 19:27:21 -0500

    Programa funcionando correctamente con extras de texto y parrafo v2

--- sinrenglones.py
+++ sinrenglones.py
@@ -13,17 +13,19 @@ print(MensajeMasivo)
 print(type(MensajeMasivo))
 
 # Dividir el mensaje en párrafos
-            parrafos = MensajeMasivo.split('\n')
+parrafos = MensajeMasivo.split('\n')
 
-            # Escribir cada párrafo en el área de texto
-            for i, parrafo in enumerate(parrafos):
-                # Escribir el párrafo actual
-                browser.find_element(By.CSS_SELECTOR, 'mws-autosize-textarea textarea').send_keys(parrafo)
+"""
+# Escribir cada párrafo en el área de texto
+for i, parrafo in enumerate(parrafos):
+    # Escribir el párrafo actual
+    browser.find_element(By.CSS_SELECTOR, 'mws-autosize-textarea textarea').send_keys(parrafo)
 
-                # Si no es el último párrafo, enviar con "Shift + Enter" para empezar uno nuevo
-                if i < len(parrafos) - 1:
-                    ActionChains(browser).key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT).perform()
+    # Si no es el último párrafo, enviar con "Shift + Enter" para empezar uno nuevo
+    if i < len(parrafos) - 1:
+        ActionChains(browser).key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT).perform()
 
-            # Enviar el último párrafo
-            browser.find_element(By.CSS_SELECTOR, 'mws-autosize-textarea textarea').send_keys(Keys.ENTER)
+# Enviar el último párrafo
+browser.find_element(By.CSS_SELECTOR, 'mws-autosize-textarea textarea').send_keys(Keys.ENTER)
 
+"""
